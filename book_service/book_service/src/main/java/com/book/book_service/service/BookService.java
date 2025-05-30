4598c6acbac1ea8a4f123d93f8a2ec98b31c6458
LeeChaeHyun <good4170@gmail.com>
2025-05-30 14:07:43 +0900

    service 구현

--- book_service/book_service/src/main/java/com/book/book_service/service/BookService.java
+++ book_service/book_service/src/main/java/com/book/book_service/service/BookService.java
@@ -1,13 +1,14 @@
 package com.book.book_service.service;
 
 import com.book.book_service.domain.Book;
+import com.book.book_service.dto.BookDTO;
 
 import java.util.List;
 
 public interface BookService {
     List<Book> findBooks();
-    Book insertBook(Book book);
+    Book insertBook(BookDTO.Post bookDTO);
     Book findBook(Long id);
-    Book updateBook(Long id, Book book);
+    Book updateBook(Long id, BookDTO.Put bookDTO);
     void deleteBook(Long id);
 }
