f853b1285aa0eeea0dda402f3b7122b8734b7ee1
LeeChaeHyun <good4170@gmail.com>
2025-05-30 10:49:04 +0900

    폴더 생성성

---
+++ book_service/book_service/src/main/java/com/book/book_service/domain/Book.java
@@ -0,0 +1,32 @@
+package com.book.book_service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy =
+            GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 64)
+    private String title;
+    @Column(nullable = false, length = 45)
+    private String subTitle;
+    @Column(length = 45)
+    private String author;
+    @Column(length = 45)
+    private String publisher;
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+    public enum Status {
+        BORROWED,
+        AVAILABLE
+    }
+}
