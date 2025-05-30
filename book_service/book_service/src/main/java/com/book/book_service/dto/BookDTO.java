fceee8527b7e3e91633aebcad3e67ef19d9b757b
gangmin <taeyoun522@gmail.com>
2025-05-30 12:16:21 +0900

    BookDTO 수정 (updateTime, uploadTime 삭제)

--- book_service/book_service/src/main/java/com/book/book_service/dto/BookDTO.java
+++ book_service/book_service/src/main/java/com/book/book_service/dto/BookDTO.java
@@ -1,12 +1,9 @@
 package com.book.book_service.dto;
 
-import com.book.book_service.domain.Book;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 public class BookDTO {
 
@@ -16,8 +13,6 @@ public class BookDTO {
     @AllArgsConstructor
     public static class Post {
         private String title;
-        private LocalDateTime uploadDate;
-        private LocalDateTime updateDate;
         private String contents;
         private String coverImage;
     }
@@ -28,8 +23,6 @@ public class BookDTO {
     @AllArgsConstructor
     public static class Put {
         private String title;
-        private LocalDateTime uploadDate;
-        private LocalDateTime updateDate;
         private String contents;
         private String coverImage;
     }
@@ -47,8 +40,6 @@ public class BookDTO {
     public static class Response {
         private Long id;
         private String title;
-        private LocalDateTime uploadDate;
-        private LocalDateTime updateDate;
         private String contents;
         private String coverImage;
     }
