7742929267cbe07705c715af5c665b65b2f56fdb
LeeChaeHyun <good4170@gmail.com>
2025-06-02 15:13:46 +0900

    User notnull 추가

--- book_service/book_service/src/main/java/com/book/book_service/domain/Users.java
+++ book_service/book_service/src/main/java/com/book/book_service/domain/Users.java
@@ -2,6 +2,7 @@ package com.book.book_service.domain;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +20,11 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
 
+    @NotNull
     @Column(unique = true, nullable = false)
     private String username;
 
+    @NotNull
     @Column(nullable = false)
     private String password;
 
